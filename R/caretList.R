760ca549ec0be7ebe9a71c8723d5607db79f2c0a
JoshD898 <jpdyce@gmail.com>
2025-01-21 23:48:34 -0800

    Reformatted according to tidyverse style guide

--- R/caretList.R
+++ R/caretList.R
@@ -1,139 +1,129 @@
-# What has changed from caretEnsemble:
-#   caretList parameters
-#   modelCheck takes in single argument instead of list
-#   improved .CheckCustomModel
-
-
-
-#' Create a caret train model for each object in dataList
+#' @title Create a list of `caret::train` objects
+#' @description This function builds a list of `caret::train` objects, where each model corresponds to a data set in `data_list`.
+#'   The resulting list is structured for use in ensembling workflows. Users can specify the training method, control parameters,
+#'   and metrics, and the function supports error handling and model trimming for efficiency.
+#' @param target Target parameter vector, either numeric for regression or a factor/character for classification.
+#' @param data_list The data sets to train models on
+#' @param method The method to train the models with. Can be a custom method or one found in `caret::modelLookup()`.
+#' @param trControl Control for use with the `caret::train` function. A default control will be constructed depending on the target type.
+#' @param metric Metric for use with `caret::train` function. A default metric will be constructed depending on the target type.
+#' @param continue_on_fail Logical, whether to skip over a data set if the model fails to train.
+#' @param trim Logical, whether the train models be trimmed to save memory and speed up stacking.
+#' @param aggregate_resamples Logical, whether to aggregate stacked predictions.
+#' @param ... Any additional arguments to pass to the `caret::train` function
+#' @return A `caret_list` object, which is a list of `caret::train` model corresponding to `data_list`.
+#'    If `continue_on_fail` is `TRUE`, the list may have fewer elements that `data_list`.
+#' @examples
+#' # TODO example code
 #'
-#'
-#'
-caretList <- function(
+#' @export
+caret_list <- function(
     target,
-    dataList,
+    data_list,
     method,
     trControl = NULL,
     metric = NULL,
-    continueOnFail = FALSE,
+    continue_on_fail = FALSE,
     trim = TRUE,
-    aggregateResamples = TRUE,
-    ...
-) {
-
-  # Basic checks
-
+    aggregate_resamples = TRUE,
+    ...) {
   if (!is.vector(target)) {
-    stop("The target must be a vector")
+    stop("The target must be a vector.")
   }
 
-  for (i in seq_along(dataList)) {
-    if (nrow(dataList[[i]]) != length(target)) {
+  for (i in seq_along(data_list)) {
+    if (nrow(data_list[[i]]) != length(target)) {
       stop(
-        "The number of rows of dataList[[", i,  "]] does not match the length of the target vector."
+        "The number of rows of data_list[[", i, "]] does not match the length of the target vector."
       )
     }
   }
 
-  .CheckMethod(method)
+  .check_method(method)
 
-  # Set trControl and metric
 
   if (is.null(trControl)) {
-    trControl <- .DefaultControl(target)
+    trControl <- .default_control(target)
   }
 
   if (is.null(metric)) {
-    metric <- .DefaultMetric(target)
+    metric <- .default_metric(target)
   }
 
-  # capture additional caret::train arguments as a list
+  train_args <- list(...)
+  train_args[["trControl"]] <- trControl
+  train_args[["metric"]] <- metric
+  train_args[["y"]] <- target
+  train_args[["method"]] <- method
 
-  trArgs <- list(...)
-  trArgs[["trControl"]] <- trControl
-  trArgs[["metric"]] <- metric
-  trArgs[["y"]] <- target
-  trArgs[["method"]] <- method
-
-  # Make model list
-
-  modelList <- lapply(
-    dataList,
-    function(data) .CaretTrain(
-      trArgs = trArgs,
-      data = data,
-      continueOnFail = FALSE,
-      trim = TRUE,
-      aggregateResamples = TRUE
-    )
+  model_list <- lapply(
+    data_list,
+    function(data) {
+      .caret_train(
+        train_args = train_args,
+        data = data,
+        continue_on_fail = FALSE,
+        trim = TRUE,
+        aggregate_resamples = TRUE
+      )
+    }
   )
   # TODO test this... what happens if training fails?
-  if (length(modelList) != length(dataList)) {
+  if (length(model_list) != length(data_list)) {
     stop("caret::train failed for one or more models. Please inspect your data.", call. = FALSE)
   }
 
-
-
-  if (is.null(names(dataList))) {
-    # assign generic names if dataList is unnamed
-    names(modelList) <- paste0("dataList[[", seq_along(dataList), "]]_model")
+  if (is.null(names(data_list))) {
+    names(model_list) <- paste0("data_list[[", seq_along(data_list), "]]_model")
   } else {
-    names(modelList) <- paste0(names(dataList), "_model")
+    names(model_list) <- paste0(names(data_list), "_model")
   }
 
+  class(model_list) <- c("caret_list", "list")
 
-  class(modelList) <- c("caretList", "list")
-
-  modelList
+  model_list
 }
 
 #' @title Wrapper to train caret models
-#' @description This function is a wrapper around the `train` function from the `caret` package.
-#' It allows for the passing of local and global arguments to the `train` function.
-#' It also allows for the option to continue on fail, and to trim the output model.
-#' Trimming the model removes components that are not needed for stacking, to save
-#' memory and speed up the stacking process. It also converts preds to a data.table.
-#' Its an internal function for use with caretList.
-#' @param trArgs A list of arguments to pass to the `train` function.
-#' @param continueOnFail A logical indicating whether to continue if the `train` function fails.
-#'  If `TRUE`, the function will return `NULL` if the `train` function fails.
+#' @description This function is a wrapper around the `caret::train`.
+#'    It allows for the option to continue on fail, and to trim the output model.
+#'    Trimming the model removes components that are not needed for stacking, to save
+#'    memory and speed up the stacking process. It also converts predictionss to a data.table.
+#' @param train_args A named list of arguments to pass to the `caret::train` function.
+#' @param data A data set to use for model training.
+#' @param continue_on_fail A logical indicating whether to continue if the `caret::train` function fails.
+#'   If `TRUE`, the function will return `NULL` if the function fails.
 #' @param trim A logical indicating whether to trim the output model.
-#' If `TRUE`, the function will remove some elements that are not needed from the output model.
-#' @param aggregateResamples A logical indicating whether to aggregate stacked predictions Default is TRUE.
-#' @return The output of the `caret::train` function.
-#' @keywords internal
-.CaretTrain <- function(
-    trArgs,
+#'   If `TRUE`, the function will remove some elements that are not needed from the output model.
+#' @param aggregate_resamples A logical indicating whether to aggregate stacked predictions Default is `TRUE`.
+#' @return A `caret::train` object or `NULL` if training fails
+#' @noRd
+.caret_train <- function(
+    train_args,
     data,
-    continueOnFail = FALSE,
+    continue_on_fail = FALSE,
     trim = TRUE,
-    aggregateResamples = TRUE
-) {
+    aggregate_resamples = TRUE) {
+  train_args[["x"]] <- data
 
-  trArgs[["x"]] <- data
-
-  # Fit model
-  if (continueOnFail) {
-    model <- tryCatch(do.call(caret::train, trArgs), error = function(e) {
+  if (continue_on_fail) {
+    model <- tryCatch(do.call(caret::train, train_args), error = function(e) {
       warning(conditionMessage(e), call. = FALSE)
       NULL
     })
   } else {
-    model <- do.call(caret::train, trArgs)
+    model <- do.call(caret::train, train_args)
   }
 
-  # Only save stacked predictions for the best model
   if ("pred" %in% names(model)) {
-    model[["pred"]] <- .ExtractBestPreds(model, aggregateResamples = aggregateResamples)
+    model[["pred"]] <- .extract_best_preds(model, aggregate_resamples = aggregate_resamples)
   }
 
   if (trim) {
-    # Remove some elements that are not needed from the final model
     if (!is.null(model[["modelInfo"]][["trim"]])) {
       model[["finalModel"]] <- model[["modelInfo"]][["trim"]](model[["finalModel"]])
     }
 
-    # Remove some elements that are not needed from the train model
     # note that caret::trim will remove stuff we DO need, such as results, preds, besttune, etc.
     removals <- c("call", "dots", "trainingData", "resampledCM")
     for (i in removals) {
@@ -142,9 +132,8 @@ caretList <- function(
       }
     }
 
-    # Remove some elements that are not needed from the model control (within the train model)
-    c_removals <- c("index", "indexOut", "indexFinal")
-    for (i in c_removals) {
+    control_removals <- c("index", "indexOut", "indexFinal")
+    for (i in control_removals) {
       if (i %in% names(model[["control"]])) {
         model[["control"]][[i]] <- NULL
       }
@@ -157,50 +146,44 @@ caretList <- function(
 
 #' @title Extract the best predictions from a train object
 #' @description Extract the best predictions from a train object.
-#' @param x a train object
-#' @param aggregateResamples logical, whether to aggregate resamples by keys. Default is TRUE.
-#' @return a data.table::data.table with predictions
-#' @keywords internal
-.ExtractBestPreds <- function(x, aggregateResamples = TRUE) {
-  stopifnot(is.logical(aggregateResamples), length(aggregateResamples) == 1L, methods::is(x, "train"))
-  if (is.null(x[["pred"]])) {
+#' @param model A `caret::train` object
+#' @param aggregate_resamples Logical, whether to aggregate resamples by keys. Default is TRUE.
+#' @return A data.table::data.table with predictions
+#' @noRd
+.extract_best_preds <- function(model, aggregate_resamples = TRUE) {
+  stopifnot(is.logical(aggregate_resamples), length(aggregate_resamples) == 1L, methods::is(x, "train"))
+
+  if (is.null(model[["pred"]])) {
     stop("No predictions saved during training. Please set savePredictions = 'final' in trControl", call. = FALSE)
   }
-  stopifnot(methods::is(x$pred, "data.frame"))
 
-  # Extract the best tune
-  keys <- names(x$bestTune)
-  best_tune <- data.table::data.table(x$bestTune, key = keys)
+  stopifnot(methods::is(model$pred, "data.frame"))
 
-  # Extract the best predictions
-  pred <- data.table::data.table(x$pred, key = keys)
+  keys <- names(model$bestTune)
+  best_tune <- data.table::data.table(model$bestTune, key = keys)
 
-  # Subset pred data to the best tune only
-  # Drop rows for other tunes
+  pred <- data.table::data.table(model$pred, key = keys)
+
   pred <- pred[best_tune, ]
 
-  # Set keys for sorting
   keys <- "rowIndex"
   data.table::setkeyv(pred, keys)
 
-  # If aggregate_resamples is TRUE, aggregate by keys
-  if (aggregateResamples) {
-    pred <- pred[, lapply(.SD, .AggregateMeanOrFirst), by = keys]
+  if (aggregate_resamples) {
+    pred <- pred[, lapply(.SD, .aggregate_mean_or_first), by = keys]
   }
 
-  # Order results consistently
   data.table::setorderv(pred, keys)
 
-  # Return
   pred
 }
 
 #' @title Aggregate mean or first
 #' @description For numeric data take the mean. For character data take the first value.
-#' @param x a train object
-#' @return a data.table::data.table with predictions
-#' @keywords internal
-.AggregateMeanOrFirst <- function(x) {
+#' @param x A vector to be aggregated.
+#' @return The mean of numeric data or the first value of non-numeric data.
+#' @noRd
+.aggregate_mean_or_first <- function(x) {
   if (is.numeric(x)) {
     mean(x)
   } else {
@@ -210,17 +193,16 @@ caretList <- function(
 
 
 #' @title Check that the method supplied by the user is a valid caret method
-#' @description Uses caret::modelLookup() to ensure the method supplied by the user
+#' @description Uses `caret::modelLookup()` to ensure the method supplied by the user
 #' is a model caret can fit
-#' @param method a user-supplied caret method or modelInfo list
+#' @param method A user-supplied caret method or modelInfo list
 #' @return NULL
-#' @keywords internal
-.CheckMethod <- function(method) {
-
+#' @noRd
+.check_method <- function(method) {
   supported_models <- unique(caret::modelLookup()[["model"]])
 
   if (is.list(method)) {
-    .CheckCustomModel(method)
+    .check_custom_model(method)
   } else if (!(method %in% supported_models)) {
     stop(
       "Method \"", method, "\" is invalid. Method must either be a character name ",
@@ -234,11 +216,10 @@ caretList <- function(
 #' @title Check that a custom caret model has required elements
 #' @description Checks for mandatory elements documented here:
 #' https://topepo.github.io/caret/using-your-own-model-in-train.html
-#' @param model the model to be validated
+#' @param model The model to be validated
 #' @return NULL
-#' @keywords internal
-.CheckCustomModel <- function(model) {
-
+#' @noRd
+.check_custom_model <- function(model) {
   required_elements <- list(
     library = "character",
     type = "character",
@@ -259,14 +240,14 @@ caretList <- function(
   }
 }
 
-#' @title Construct a default metric for use with caretList
-#' @description Caret defaults to RMSE for classification and RMSE for regression.
-#' For classification, I would rather use ROC.
-#' @param target Target parameter vector
-#' @return TODO
-#' @keywords internal
-.DefaultMetric <- function(target) {
-
+#' @title Construct a default metric for use with caret_list
+#' @description Determines the default metric based on the type of target vector.
+#' For classification, defaults to "ROC" if the target is binary and "Accuracy" otherwise.
+#' For regression, defaults to "RMSE".
+#' @param target Target parameter vector, either numeric for regression or a factor/character for classification.
+#' @return A character string indicating the default metric
+#' @noRd
+.default_metric <- function(target) {
   is_class <- is.factor(target) || is.character(target)
   is_binary <- length(unique(target)) == 2L
 
@@ -281,17 +262,16 @@ caretList <- function(
   }
 }
 
-#' @title Construct a default train control for use with caretList
-#' @description Unlike caret::trainControl, this function defaults to 5 fold CV.
+#' @title Construct a default train control for use with caret_list
+#' @description Unlike `caret::trainControl()`, this function defaults to 5 fold CV.
 #' CV is good for stacking, as every observation is in the test set exactly once.
-#' We use 5 instead of 10 to save compute time, as caretList is for fitting many
-#' models. We also construct explicit fold indexes and return the stacked predictions,
-#' which are needed for stacking. For classification models we return class probabilities.
-#' @param target Target parameter vector
-#' @return TODO
-#' @keywords internal
-.DefaultControl <- function(target) {
-
+#' 5 is used instead of 10 to save compute time, as caret_list is for fitting many
+#' models. Explicit fold indexes are constructed, and predictions are saved for stacking.
+#' For classification models, class probabilities are returned.
+#' @param target Target parameter vector, either numeric for regression or a factor/character for classification.
+#' @return A `caret::trainControl` object
+#' @noRd
+.default_control <- function(target) {
   is_class <- is.factor(target) || is.character(target)
   is_binary <- length(unique(target)) == 2L
 
@@ -303,6 +283,5 @@ caretList <- function(
     classProbs = is_class,
     summaryFunction = ifelse(is_class && is_binary, caret::twoClassSummary, caret::defaultSummary),
     returnData = FALSE
-    )
+  )
 }
-
