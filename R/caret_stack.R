cdac0f2f5d5312e7e5bd72cdd91ac94c108e5a2e
JoshD898 <jpdyce@gmail.com>
2025-02-06 15:51:30 -0800

    Made outline for caret_stack constructor and methods

---
+++ R/caret_stack.R
@@ -0,0 +1,46 @@
+
+caret_stack <- function() {
+
+}
+
+
+# Methods ----------------------------------------------------------------------
+
+summary.caret_stack <- function() {
+
+}
+
+print.caret_stack <- function() {
+
+}
+
+
+print.summary.caret_stack <- function() {
+
+}
+
+extract_metric.caret_stack <- function() {
+
+}
+
+dotplot.caret_stack <- function() {
+
+}
+
+autoplot.caret_stack <- function() {
+
+}
+
+plot.caret_stack <- function() {
+
+}
+
+
+
+
+
+
+
+
+
+# Helper functions -------------------------------------------------------------
